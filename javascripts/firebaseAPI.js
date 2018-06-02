3641cdb2fc1b4d96664ebc8bdbd6c1d59c9a450a
Barry Russell <blrussell09@gmail.com>
2018-06-02 11:47:32 -0500

    Added functions to myMovie buttons

--- javascripts/firebaseAPI.js
+++ javascripts/firebaseAPI.js
@@ -45,6 +45,50 @@ const getAllMovies = () => {
   });
 };
 
+const getWatchedMovies = () => {
+  return new Promise((resolve, reject) => {
+    const allMoviesArray = [];
+    $.ajax({
+      method: 'GET',
+      url: `${firebaseConfig.databaseURL}/movies.json?orderBy="isWatched"&equalTo=true`,
+    })
+      .done((allMoviesObj) => {
+        if (allMoviesObj !== null) {
+          Object.keys(allMoviesObj).forEach((fbKey) => {
+            allMoviesObj[fbKey].id = fbKey;
+            allMoviesArray.push(allMoviesObj[fbKey]);
+          });
+        }
+        resolve(allMoviesArray);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+const getWishlistMovies = () => {
+  return new Promise((resolve, reject) => {
+    const allMoviesArray = [];
+    $.ajax({
+      method: 'GET',
+      url: `${firebaseConfig.databaseURL}/movies.json?orderBy="isWatched"&equalTo=false`,
+    })
+      .done((allMoviesObj) => {
+        if (allMoviesObj !== null) {
+          Object.keys(allMoviesObj).forEach((fbKey) => {
+            allMoviesObj[fbKey].id = fbKey;
+            allMoviesArray.push(allMoviesObj[fbKey]);
+          });
+        }
+        resolve(allMoviesArray);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
 const deleteMovieFromDb = (movieId) =>
 {
   return new Promise ((resolve, reject) =>
@@ -88,6 +132,8 @@ module.exports = {
   setConfig,
   saveMovieToWishList,
   getAllMovies,
+  getWatchedMovies,
+  getWishlistMovies,
   deleteMovieFromDb,
   updateMovieToWatched,
 };
